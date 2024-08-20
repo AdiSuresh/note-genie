c188d95e7f99c271bb8ae367816cf96b2b19ab99
Adithya Suresh <50865181+AdiSuresh@users.noreply.github.com>
2024-08-20 23:54:27 +0530

    remove InkWell in CollectionChip widget + anim. when selecting collection (#11)

--- lib/views/home/widgets/collection_chip.dart
+++ lib/views/home/widgets/collection_chip.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:note_maker/app/themes/themes.dart';
 
 class CollectionChip extends StatelessWidget {
   final VoidCallback onTap;
@@ -13,11 +12,10 @@ class CollectionChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.zero,
-      child: InkWell(
-        customBorder: Themes.shape,
-        onTap: onTap,
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: EdgeInsets.zero,
         child: Padding(
           padding: const EdgeInsets.all(15),
           child: child,
