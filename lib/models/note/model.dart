8af76c309958862a20c39988fb7cf405486e6147
Adithya Suresh <adithyasuresh201@gmail.com>
2024-08-26 19:25:49 +0530

    create NoteEntity.empty() constructor & use it in router

--- lib/models/note/model.dart
+++ lib/models/note/model.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:json_annotation/json_annotation.dart';
@@ -80,6 +79,14 @@ class NoteEntity implements BaseEntity {
     required this.content,
     required this.collections,
   });
+
+  factory NoteEntity.empty() {
+    return NoteEntity(
+      title: 'Untitled',
+      content: '',
+      collections: ob.ToMany(),
+    );
+  }
 
   String get contentAsText {
     return Document.fromJson(
