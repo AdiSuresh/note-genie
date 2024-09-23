c70ce7a4773d5594f68ff988efa4b7bdc325bced
Adithya Suresh <50865181+AdiSuresh@users.noreply.github.com>
2024-09-23 23:30:34 +0530

    Handle pop to prev. screen in edit note screen (#26)
    * remove note collection sub + replace 'pop' with 'go' on delete note + create NoteCollectionListSheet + refactor
    
    * handle pop to prev. screen in edit note screen + close sheet on editor focus + refactor
    
    * change back duration to animationDuration

--- lib/views/edit_note/view.dart
+++ lib/views/edit_note/view.dart
@@ -8,15 +8,13 @@ import 'package:note_maker/app/logger.dart';
 import 'package:note_maker/data/objectbox_db.dart';
 import 'package:note_maker/models/note/model.dart';
 import 'package:note_maker/models/note_collection/model.dart';
-import 'package:note_maker/objectbox.g.dart';
 import 'package:note_maker/utils/extensions/build_context.dart';
 import 'package:note_maker/utils/ui_utils.dart';
 import 'package:note_maker/utils/text_input_validation/validators.dart';
 import 'package:note_maker/views/edit_note/bloc.dart';
 import 'package:note_maker/views/edit_note/event.dart';
 import 'package:note_maker/views/edit_note/state/state.dart';
-import 'package:note_maker/views/home/widgets/no_collections_message.dart';
-import 'package:note_maker/widgets/collection_list_tile.dart';
+import 'package:note_maker/views/edit_note/widgets/note_collection_list_sheet.dart';
 import 'package:note_maker/widgets/dismiss_keyboard.dart';
 
 class EditNote extends StatefulWidget {
@@ -31,8 +29,12 @@ class EditNote extends StatefulWidget {
 }
 
 class _EditNoteState extends State<EditNote> {
-  final logger = AppLogger(
+  static final logger = AppLogger(
     EditNote,
+  );
+
+  static const animationDuration = Duration(
+    milliseconds: 250,
   );
 
   final db = ObjectBoxDB();
@@ -45,7 +47,6 @@ class _EditNoteState extends State<EditNote> {
   final contentScrollCtrl = ScrollController();
 
   StreamSubscription<DocChange>? docChangesSub;
-  StreamSubscription<List<NoteCollectionEntity>>? noteCollectionsSub;
 
   final sheetCtrl = DraggableScrollableController();
 
@@ -66,7 +67,6 @@ class _EditNoteState extends State<EditNote> {
           documentJson,
         ),
     };
-    startNoteCollectionsSub();
     docChangesSub = document.changes.listen(
       (event) async {
         logger.d(
@@ -78,7 +78,6 @@ class _EditNoteState extends State<EditNote> {
             note: note,
           ),
         );
-        startNoteCollectionsSub();
       },
     );
     contentCtrl = QuillController(
@@ -87,52 +86,29 @@ class _EditNoteState extends State<EditNote> {
         offset: 0,
       ),
     );
+    contentFocus.addListener(
+      onEditorFocus,
+    );
   }
 
   @override
   void dispose() {
     titleCtrl.dispose();
     contentCtrl.dispose();
+    contentFocus.removeListener(
+      onEditorFocus,
+    );
     contentFocus.dispose();
     contentScrollCtrl.dispose();
-    docChangesSub?.cancel().whenComplete(
-      () {
-        logger.i(
-          'changesSub disposed',
-        );
-      },
-    );
-    noteCollectionsSub?.cancel();
+    docChangesSub?.cancel();
+    sheetCtrl.dispose();
     super.dispose();
   }
 
-  Future<void> startNoteCollectionsSub() async {
-    if (note.id > 0 && noteCollectionsSub == null) {
-      final store = await db.store;
-      final builder = store.box<NoteCollectionEntity>().query()
-        ..linkMany(
-          NoteCollectionEntity_.notes,
-          NoteEntity_.id.equals(
-            note.id,
-          ),
-        );
-      noteCollectionsSub = builder
-          .watch(
-            triggerImmediately: true,
-          )
-          .map(
-            (query) => query.find(),
-          )
-          .listen(
-        (noteCollections) {
-          bloc.add(
-            UpdateNoteCollectionsEvent(
-              noteCollections: noteCollections,
-            ),
-          );
-        },
-      );
-    }
+  void onEditorFocus() {
+    sheetCtrl.jumpTo(
+      0,
+    );
   }
 
   Future<NoteEntity> saveContent() async {
@@ -212,26 +188,26 @@ class _EditNoteState extends State<EditNote> {
             );
       },
     );
+    if (deleted) {
+      logger.d(
+        'deleted note with id: ${note.id}',
+      );
+      docChangesSub?.pause();
+    }
     final title = "'${note.title}'";
     final content = switch (deleted) {
-      true => () {
-          logger.d(
-            'deleted note with id: ${note.id}',
-          );
-          docChangesSub?.pause();
-          return '$title was deleted successfully';
-        },
-      _ => () {
-          return 'Could not delete $title';
-        },
-    }();
+      true => '$title was deleted successfully',
+      _ => 'Could not delete $title',
+    };
     if (mounted) {
       UiUtils.showSnackbar(
         context,
         content: content,
         onClose: () {
-          if (deleted) {
-            context.pop();
+          if (deleted && mounted) {
+            context.go(
+              '/',
+            );
           }
         },
       );
@@ -266,214 +242,152 @@ class _EditNoteState extends State<EditNote> {
 
   @override
   Widget build(BuildContext context) {
-    return DismissKeyboard(
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: InkWell(
-            onTap: renameNote,
-            borderRadius: BorderRadius.circular(7.5),
-            child: Padding(
-              padding: const EdgeInsets.all(7.5),
-              child: BlocBuilder<EditNoteBloc, EditNoteState>(
-                buildWhen: (previous, current) {
-                  final t1 = previous.note.title;
-                  final t2 = current.note.title;
-                  return t1 != t2;
-                },
-                builder: (context, state) {
-                  return Text(
-                    state.note.title,
-                    style: context.themeData.textTheme.titleLarge,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  );
-                },
-              ),
-            ),
-          ),
-          actions: [
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 5,
-              ),
-              child: Text(
-                'Saving...',
-              ),
-            ),
-            PopupMenuButton(
-              color: Colors.white,
-              surfaceTintColor: Colors.white,
-              onOpened: () {
-                contentFocus.unfocus();
-              },
-              itemBuilder: (context) {
-                const style = TextStyle(
-                  fontWeight: FontWeight.normal,
-                );
-                return [
-                  PopupMenuItem(
-                    child: const Text(
-                      'Collections',
-                      style: style,
-                    ),
-                    onTap: () async {
-                      await sheetCtrl.animateTo(
-                        1,
-                        duration: const Duration(
-                          milliseconds: 150,
-                        ),
-                        curve: Curves.ease,
-                      );
-                      switch (this.context) {
-                        case BuildContext context when context.mounted:
-                          UiUtils.dismissKeyboard(
-                            context,
-                          );
-                        case _:
-                      }
-                    },
-                  ),
-                  PopupMenuItem(
-                    child: const Text(
-                      'Linked Notes',
-                      style: style,
-                    ),
-                    onTap: () {},
-                  ),
-                  PopupMenuItem(
-                    child: Text(
-                      'Delete',
-                      style: style.copyWith(
-                        color: Colors.red,
-                      ),
-                    ),
-                    onTap: () {
-                      UiUtils.showProceedDialog(
-                        title: 'Delete note?',
-                        message: 'You are about to delete this note.'
-                            ' Once deleted its gone forever.'
-                            '\n\nAre you sure you want to proceed?',
-                        context: context,
-                        onYes: () {
-                          context.pop();
-                          deleteNote();
-                        },
-                        onNo: () {
-                          context.pop();
-                        },
-                      );
-                    },
-                  ),
-                ];
-              },
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              QuillEditor(
-                configurations: const QuillEditorConfigurations(
-                  padding: EdgeInsets.all(15),
-                  scrollPhysics: BouncingScrollPhysics(),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) {
+          return;
+        }
+        if (sheetCtrl.pixels > 0) {
+          sheetCtrl.animateTo(
+            0,
+            duration: animationDuration,
+            curve: Curves.ease,
+          );
+          return;
+        }
+        if (contentFocus.hasFocus) {
+          contentFocus.unfocus();
+          return;
+        }
+        if (mounted) {
+          context.pop(
+            result,
+          );
+        }
+      },
+      child: DismissKeyboard(
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: InkWell(
+              onTap: renameNote,
+              borderRadius: BorderRadius.circular(7.5),
+              child: Padding(
+                padding: const EdgeInsets.all(7.5),
+                child: BlocBuilder<EditNoteBloc, EditNoteState>(
+                  buildWhen: (previous, current) {
+                    final t1 = previous.note.title;
+                    final t2 = current.note.title;
+                    return t1 != t2;
+                  },
+                  builder: (context, state) {
+                    return Text(
+                      state.note.title,
+                      style: context.themeData.textTheme.titleLarge,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    );
+                  },
                 ),
-                focusNode: contentFocus,
-                scrollController: contentScrollCtrl,
-                controller: contentCtrl,
               ),
-              DraggableScrollableSheet(
-                minChildSize: 0,
-                initialChildSize: 0,
-                controller: sheetCtrl,
-                snapSizes: const [
-                  0,
-                ],
-                snap: true,
-                builder: (context, scrollController) {
-                  return BlocBuilder<EditNoteBloc, EditNoteState>(
-                    buildWhen: (previous, current) {
-                      return true;
-                    },
-                    builder: (context, state) {
-                      final collections = state.note.collections;
-                      final children = <Widget>[
-                        ListView(
-                          controller: scrollController,
-                          padding: const EdgeInsets.all(15),
-                          children: collections.map(
-                            (collection) {
-                              return CollectionListTile(
-                                collection: collection,
-                                onRemoveNote: () {
-                                  final collectionTitle =
-                                      "'${collection.name}'";
-                                  // final context = this.context;
-                                  UiUtils.showProceedDialog(
-                                    title: 'Remove note from collection?',
-                                    message: 'You are about to this note from'
-                                        ' $collectionTitle.'
-                                        '\n\nAre you sure you want to proceed?',
-                                    context: context,
-                                    onYes: () {
-                                      context.pop();
-                                      removeFromCollection(
-                                        collection,
-                                      );
-                                    },
-                                    onNo: () {
-                                      context.pop();
-                                    },
-                                  );
-                                },
-                              );
-                            },
-                          ).toList(),
-                        )
-                      ];
-                      if (collections case []) {
-                        children.add(
-                          const Center(
-                            child: NoCollectionsMessage(),
-                          ),
-                        );
-                      }
-                      return Container(
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(15),
-                          ),
-                        ),
-                        child: Stack(
-                          children: children,
-                        ),
-                      );
-                    },
+            ),
+            actions: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 5,
+                ),
+                child: Text(
+                  'Saving...',
+                ),
+              ),
+              PopupMenuButton(
+                color: Colors.white,
+                surfaceTintColor: Colors.white,
+                onOpened: () {
+                  contentFocus.unfocus();
+                },
+                itemBuilder: (context) {
+                  const style = TextStyle(
+                    fontWeight: FontWeight.normal,
                   );
+                  return [
+                    PopupMenuItem(
+                      child: const Text(
+                        'Collections',
+                        style: style,
+                      ),
+                      onTap: () async {
+                        await sheetCtrl.animateTo(
+                          1,
+                          duration: animationDuration,
+                          curve: Curves.ease,
+                        );
+                        switch (this.context) {
+                          case BuildContext context when context.mounted:
+                            UiUtils.dismissKeyboard(
+                              context,
+                            );
+                          case _:
+                        }
+                      },
+                    ),
+                    PopupMenuItem(
+                      child: const Text(
+                        'Linked Notes',
+                        style: style,
+                      ),
+                      onTap: () {},
+                    ),
+                    PopupMenuItem(
+                      child: Text(
+                        'Delete',
+                        style: style.copyWith(
+                          color: Colors.red,
+                        ),
+                      ),
+                      onTap: () {
+                        final context = this.context;
+                        UiUtils.showProceedDialog(
+                          title: 'Delete note?',
+                          message: 'You are about to delete this note.'
+                              ' Once deleted its gone forever.'
+                              '\n\nAre you sure you want to proceed?',
+                          context: context,
+                          onYes: () {
+                            context.pop();
+                            deleteNote();
+                          },
+                          onNo: () {
+                            context.pop();
+                          },
+                        );
+                      },
+                    ),
+                  ];
                 },
               ),
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            final store = await db.store;
-            store.box<NoteCollectionEntity>().getAll().forEach(
-              (element) {
-                note.collections
-                  ..add(
-                    element,
-                  )
-                  ..applyToDb();
-              },
-            );
-            bloc.add(
-              UpdateNoteEvent(
-                note: note,
-              ),
-            );
-          },
+          body: SafeArea(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                QuillEditor(
+                  configurations: const QuillEditorConfigurations(
+                    padding: EdgeInsets.all(15),
+                    scrollPhysics: BouncingScrollPhysics(),
+                  ),
+                  focusNode: contentFocus,
+                  scrollController: contentScrollCtrl,
+                  controller: contentCtrl,
+                ),
+                NoteCollectionListSheet(
+                  controller: sheetCtrl,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
