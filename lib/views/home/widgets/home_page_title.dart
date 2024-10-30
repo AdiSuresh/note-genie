ae398a053a5308a37bb28aa9531b5412aa308a89
Adithya Suresh <50865181+AdiSuresh@users.noreply.github.com>
2024-10-30 23:44:18 +0530

    fix search field render errors + create page title widget + refactor (#46)

---
+++ lib/views/home/widgets/home_page_title.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_maker/utils/extensions/build_context.dart';
+import 'package:note_maker/views/home/bloc.dart';
+import 'package:note_maker/views/home/state/state.dart';
+
+class HomePageTitle extends StatelessWidget {
+  const HomePageTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      buildWhen: (previous, current) {
+        switch ((previous, current)) {
+          case (IdleState previous, IdleState current):
+            return previous.pageTitle != current.pageTitle;
+          case (_, IdleState _):
+            return true;
+          case _:
+        }
+        return false;
+      },
+      builder: (context, state) {
+        if (state case IdleState _) {
+          return Text(
+            state.pageTitle,
+            style: context.themeData.textTheme.titleLarge,
+          );
+        }
+        return const SizedBox();
+      },
+    );
+  }
+}
