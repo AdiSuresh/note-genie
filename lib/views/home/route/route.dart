787c227186090ff8f34e73aba145c63d4c03f9d2
Adithya Suresh <adithyasuresh201@gmail.com>
2024-11-28 23:46:23 +0530

    use typed routes

---
+++ lib/views/home/route/route.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:note_maker/app/router/blocs/navigation/bloc.dart';
+import 'package:note_maker/utils/ui_utils.dart';
+import 'package:note_maker/views/edit_note/route/route.dart';
+import 'package:note_maker/views/home/bloc.dart';
+import 'package:note_maker/views/home/event.dart';
+import 'package:note_maker/views/home/repository.dart';
+import 'package:note_maker/views/home/state/state.dart';
+import 'package:note_maker/views/home/view.dart';
+
+part 'route.g.dart';
+
+@TypedGoRoute<HomeRoute>(
+  path: HomePage.path,
+  routes: <TypedGoRoute<GoRouteData>>[
+    TypedGoRoute<EditNoteRoute>(
+      path: 'edit-note',
+    ),
+  ],
+)
+class HomeRoute extends GoRouteData {
+  HomeBloc? _bloc;
+
+  HomeRoute();
+
+  @override
+  FutureOr<bool> onExit(BuildContext context, GoRouterState state) async {
+    switch (_bloc) {
+      case HomeBloc(
+          state: SearchState(),
+          :final add,
+        ):
+        add(
+          ToggleSearchEvent(),
+        );
+        return false;
+      case _:
+    }
+    final exit = await UiUtils.showProceedDialog(
+      title: 'Exit app?',
+      message: 'Would you like to exit the app?',
+      context: context,
+      onYes: () {
+        context.pop(
+          true,
+        );
+      },
+      onNo: () {
+        context.pop(
+          false,
+        );
+      },
+    );
+    return switch (exit) {
+      bool() => exit,
+      _ => false,
+    };
+  }
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    final provider = MultiBlocProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) {
+            return HomeRepository();
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            final result = HomeBloc(
+              const IdleState(
+                notes: [],
+                noteCollections: [],
+              ),
+              context.read<NavigationBloc>(),
+              repository: context.read<HomeRepository>(),
+              path: HomePage.path,
+            );
+            _bloc = result;
+            return result;
+          },
+        ),
+      ],
+      child: const HomePage(),
+    );
+    return PopScope(
+      canPop: false,
+      child: provider,
+    );
+  }
+}
