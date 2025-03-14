8e649aec2c54b38b75e69f41cae80924864f5479
ADITHYA SURESH <adithyasuresh201@gmail.com>
2025-03-15 00:35:25 +0530

    title update logic

--- lib/views/chat/widgets/page_title.dart
+++ lib/views/chat/widgets/page_title.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_maker/models/chat/model.dart';
+import 'package:note_maker/models/future_data/model.dart';
 import 'package:note_maker/utils/extensions/build_context.dart';
 import 'package:note_maker/views/chat/bloc.dart';
 import 'package:note_maker/views/chat/state/state.dart';
+import 'package:note_maker/widgets/custom_animated_switcher.dart';
 
 class ChatPageTitle extends StatelessWidget {
   const ChatPageTitle({
@@ -11,17 +14,61 @@ class ChatPageTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = context.read<ChatBloc>();
     return BlocBuilder<ChatBloc, ChatState>(
       buildWhen: (previous, current) {
+        switch ((previous, current)) {
+          case (
+              IdleState(
+                chat: AsyncData(
+                  data: final c1,
+                ),
+              ),
+              IdleState(
+                chat: AsyncData(
+                  data: final c2,
+                ),
+              ),
+            ):
+            return c1.remoteId != c2.remoteId || c1.title != c2.title;
+          case _:
+        }
         return false;
       },
       builder: (context, state) {
-        final pageTitle = 'Chat';
-        return Text(
+        final chat = bloc.idleState.chat.data;
+        final pageTitle = switch (chat) {
+          ChatModel(
+            remoteId: null,
+          ) =>
+            'New Chat',
+          _ => chat.title,
+        };
+        final child = Text(
           pageTitle,
           style: context.themeData.textTheme.titleLarge,
           maxLines: 1,
           overflow: TextOverflow.ellipsis,
+        );
+        return CustomAnimatedSwitcher(
+          child: switch (chat) {
+            ChatModel(
+              remoteId: null,
+            ) =>
+              child,
+            _ => TextButton.icon(
+                onPressed: () {},
+                label: Row(
+                  children: [
+                    child,
+                    Icon(
+                      Icons.arrow_drop_down_rounded,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ),
+          },
         );
       },
     );
