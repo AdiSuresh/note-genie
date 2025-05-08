5c7665acb845dc213a4a13c494abce6342c88be7
Adithya Suresh <adithyasuresh201@gmail.com>
2025-05-08 13:50:35 +0530

    implement ThreeDotIndicator

--- lib/widgets/three_dot_indicator.dart
+++ lib/widgets/three_dot_indicator.dart
@@ -3,13 +3,11 @@ import 'package:note_maker/utils/extensions/tween.dart';
 
 class ThreeDotIndicator extends StatefulWidget {
   final Duration duration;
-  final double minSize;
 
   const ThreeDotIndicator({
     super.key,
-    this.minSize = 0.75,
     this.duration = const Duration(
-      milliseconds: 500,
+      milliseconds: 125,
     ),
   });
 
@@ -18,12 +16,44 @@ class ThreeDotIndicator extends StatefulWidget {
 }
 
 class _ThreeDotIndicatorState extends State<ThreeDotIndicator> {
+  static const s0 = (false, false, false);
+  static const s1 = (true, false, false);
+  static const s2 = (false, true, false);
+  static const s3 = (false, false, true);
+
+  var state = s1;
+
+  void mutate() {
+    switch (state) {
+      case s0:
+        forward = !forward;
+        if (forward) {
+          state = s1;
+        }
+      case s1:
+        forward = !forward;
+        if (forward) {
+          state = s2;
+        }
+      case s2:
+        forward = !forward;
+        if (forward) {
+          state = s3;
+        }
+      case _:
+        forward = !forward;
+        if (forward) {
+          state = s0;
+        }
+    }
+  }
+
   bool forward = true;
 
   @override
   Widget build(BuildContext context) {
     final tween = Tween(
-      begin: widget.minSize,
+      begin: 0.0,
       end: 1.0,
     );
     return TweenAnimationBuilder(
@@ -34,20 +64,33 @@ class _ThreeDotIndicatorState extends State<ThreeDotIndicator> {
       duration: widget.duration,
       curve: Curves.ease,
       builder: (context, value, child) {
+        final (s1, s2, s3) = state;
         return Stack(
           alignment: Alignment.center,
           children: [
             const SizedBox.square(
-              dimension: 15,
+              dimension: 7.5,
             ),
-            DecoratedBox(
-              decoration: BoxDecoration(
-                color: Colors.black,
-                shape: BoxShape.circle,
-              ),
-              child: SizedBox.square(
-                dimension: value * 15,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _Dot(
+                  yOffset: s1 ? -5 * value : 0.0,
+                ),
+                const SizedBox(
+                  width: 2.5,
+                ),
+                _Dot(
+                  yOffset: s2 ? -5 * value : 0.0,
+                ),
+                const SizedBox(
+                  width: 2.5,
+                ),
+                _Dot(
+                  yOffset: s3 ? -5 * value : 0.0,
+                ),
+              ],
             ),
           ],
         );
@@ -57,9 +100,40 @@ class _ThreeDotIndicatorState extends State<ThreeDotIndicator> {
           return;
         }
         setState(() {
-          forward = !forward;
+          mutate();
+          print('state: $state');
         });
       },
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  final double yOffset;
+
+  const _Dot({
+    required this.yOffset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.identity()
+        ..translate(
+          0.0,
+          yOffset,
+          0.0,
+        ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.black45,
+          shape: BoxShape.circle,
+        ),
+        child: const SizedBox.square(
+          dimension: 7.5,
+        ),
+      ),
     );
   }
 }
