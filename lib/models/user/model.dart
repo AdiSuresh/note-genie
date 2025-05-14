a21229acf753d7595867f54aea34d6e66040cdb5
Adithya Suresh <adithyasuresh201@gmail.com>
2025-05-14 10:56:50 +0530

    add json ser.

--- lib/models/user/model.dart
+++ lib/models/user/model.dart
@@ -1,7 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'model.g.dart';
+
+@JsonSerializable()
 final class User {
   final String email;
 
   const User({
     required this.email,
   });
+
+  factory User.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return _$UserFromJson(
+      json,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$UserToJson(
+      this,
+    );
+  }
 }
