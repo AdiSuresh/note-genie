c3ffdc78bfc93ecb987cfa7c8639ed6a8025cc21
Adithya Suresh <adithyasuresh201@gmail.com>
2025-05-14 10:57:48 +0530

    save & retrieve user from secure storage

--- lib/services/token_manager.dart
+++ lib/services/token_manager.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:note_maker/models/user/model.dart';
 
 class TokenManager {
   static const String _tokenKey = 'access_token';
+  static const String _userKey = 'user';
 
   static final TokenManager _instance = TokenManager._();
 
@@ -10,6 +14,35 @@ class TokenManager {
   factory TokenManager() => _instance;
 
   TokenManager._();
+
+  Future<void> saveUser(
+    User user,
+  ) async {
+    await _storage.write(
+      key: _userKey,
+      value: json.encode(
+        user.toJson(),
+      ),
+    );
+  }
+
+  Future<User?> getUser() async {
+    final data = await _storage.read(
+      key: _userKey,
+    );
+    try {
+      final decoded = json.decode(
+        data!,
+      );
+      final user = User.fromJson(
+        decoded,
+      );
+      return user;
+    } catch (e) {
+      // ignored
+    }
+    return null;
+  }
 
   Future<void> saveAccessToken(
     String token,
