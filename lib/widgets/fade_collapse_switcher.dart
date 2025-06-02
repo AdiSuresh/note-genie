45584e5c4a7354936582516fc2190b59ce83aab3
Adithya Suresh <adithyasuresh201@gmail.com>
2025-06-02 15:53:31 +0530

    create FadeCollapseSwitcher

---
+++ lib/widgets/fade_collapse_switcher.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+
+class FadeCollapseSwitcher extends StatelessWidget {
+  final bool visibility;
+  final Axis axis;
+  final Widget? child;
+
+  const FadeCollapseSwitcher({
+    super.key,
+    required this.visibility,
+    this.axis = Axis.vertical,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(
+        milliseconds: 125,
+      ),
+      transitionBuilder: (child, animation) {
+        final t1 = FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+        final t2 = SizeTransition(
+          sizeFactor: animation,
+          axis: axis,
+          child: t1,
+        );
+        return t2;
+      },
+      child: switch (visibility) {
+        true => child,
+        _ => const SizedBox(),
+      },
+    );
+  }
+}
